dd22b81db5eeb60cc4e414536c431736606e8324
rayhanbri <mohammadrayhanx0@gmail.com>
2025-04-13 00:14:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    {/* ctrl + space for tailwind suggestion  */}
+     
+      <h1 className='text-3xl'>Vite + React</h1>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
+      
+     
